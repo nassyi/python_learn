669c08fd62c6b8b57b78305cc1c233cb55c1078e
nassyi <raspberrycookie1@gmail.com>
2023-08-12 15:08:03 +0900

    11まで

--- streamlit/main.py
+++ streamlit/main.py
@@ -7,9 +7,16 @@ st.title('streamlit 入門')
 
 st.write('インタラクティブウィジェット')
 
+
+left_column, right_column = st.columns(2)
+button = left_column.button('左カラムに文字を表示')
+if button:
+    right_column.write('ここは右カラム')
+
+exp = st.expander('問い合わせ')
+exp.write('問い合わせ内容を書く')
+
 option = st.text_input('趣味を教えて')
-
-
 'あなたの好きな趣味は、', option, 'です'
 
 condition = st.slider('あなたの今の調子は？', 0, 100, 50)
